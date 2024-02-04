12bd722279eee826d778a9beca184381edcb8ede
lukas <lukas.machala@allodium.eu>
2024-02-04 20:51:23 +0100

    unecessary buttons removal

--- src/components/action/index.tsx
+++ src/components/action/index.tsx
@@ -1,29 +1,14 @@
 'use client'
 
 import { api } from '~/trpc/react'
-import { dispatchSidebarLeftToggle } from '~/components/menu-left'
-import { dispatchSidebarRightToggle } from '~/components/menu-right'
 
-import { Button as ComponentButton } from '~/components/button/component'
-import { Button as ShadcnButton } from '~/components/button/shadcn'
 import { Button } from '../button/shadcn'
 
 export default function Action() {
   const { data } = api.game.position.useQuery()
 
-  function toggleSidebars() {
-    dispatchSidebarLeftToggle()
-    dispatchSidebarRightToggle()
-  }
-
   return (
     <>
-      <ShadcnButton variant='destructive'>ShadcnButton</ShadcnButton>
-      <ComponentButton variant='primary'>Button</ComponentButton>
-      <button className='text-gray-300 bg-slate-800 p-5' onClick={toggleSidebars}>
-        RegularButton - Toggle Sidebars
-      </button>
-
       {data?.enemy && (
         <div className='flex justify-between'>
           <Button>Útok</Button>
