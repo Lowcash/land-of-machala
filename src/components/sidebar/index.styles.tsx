b8be2cd9bcf87d4594eff0153db2e4cd67639314
lukas <lukas.machala@allodium.eu>
2024-07-14 22:08:33 +0200

    new sidebar style

--- src/components/sidebar/index.styles.tsx
+++ src/components/sidebar/index.styles.tsx
@@ -5,50 +5,48 @@ import { BaseDirection } from '@/const'
 import tw from 'twin.macro'
 import styled from '@emotion/styled/macro'
 
-type SidebarProps = {
+import { H2, H3 } from '@/styles/text'
+
+interface SidebarProps {
   open: boolean
-  direction: BaseDirection
+  position: BaseDirection
 }
 
-const _SidebarInner = styled('div')(
-  tw`
-    flex flex-col
-    h-full
-    px-3 py-4 
+const _SidebarInner = styled('div')`
+  ${tw`
+    flex flex-col h-full px-3 py-4 
     overflow-y-auto
-    border-slate-200 bg-white dark:border-slate-700 dark:bg-slate-900
-  `
-)
+    rounded-lg border-4
+  `}
 
-const DIRECTION_MAP: Record<BaseDirection, { position: any; border: any }> = {
-  left: {
-    position: tw`left-0`,
-    border: tw`border-r`,
-  },
-  right: {
-    position: tw`right-0`,
-    border: tw`border-l`,
-  },
-}
+  background: #f0e6d2;
+`
 
 const _SidebarOuter = styled('aside', {
   shouldForwardProp: (p) => p !== 'open' && p !== 'direction',
 })<SidebarProps>`
   height: calc(100vh - 72px);
   top: 36px;
-  
-  ${tw`
-    fixed
 
-    transition-transform
-    z-40
-  `}
+  ${tw`fixed z-40`}
 
   ${({ open }) => (open ? tw`w-64` : tw`hidden`)}
-  ${({ direction }) => DIRECTION_MAP[direction].position}
+  ${({ position }) => (
+    position === 'left' 
+    ? tw`left-0` 
+    : position === 'right' 
+    ? tw`right-0` 
+    : ''
+  )}
 
   > ${_SidebarInner} {
-    ${({ direction }) => DIRECTION_MAP[direction].border}
+    ${({ position }) => (
+      position === 'left'
+      ? `box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);` :
+      position === 'right'
+      ? `box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5)`
+      : ''
+    )}
   }
 `
 
@@ -57,3 +55,21 @@ export const Sidebar = ({ children, ...sidebar }: React.PropsWithChildren<Sideba
     <_SidebarInner>{children}</_SidebarInner>
   </_SidebarOuter>
 )
+
+Sidebar.Header = styled(H2)`
+  ${tw`mb-4`}
+`
+
+Sidebar.Section = styled('div')``
+Sidebar.SectionHeader = styled(H3)`
+  ${tw`text-gray-900`}
+  background: rgba(0, 0, 0, 0.1);
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+`
+Sidebar.SectionContent = styled('div')`
+  ${tw`space-y-2`}
+  margin-left: 1rem;
+  padding-left: 0.5rem;
+  border-left: 2px solid rgba(0, 0, 0, 0.1);
+`
