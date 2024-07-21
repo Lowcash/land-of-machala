290d7403c445fc94f1ab33c0911d9e9eed2d9dae
lukas <lukas.machala@allodium.eu>
2024-07-21 16:07:54 +0200

    spa solution for page routing

--- src/const.ts
+++ src/const.ts
@@ -17,12 +17,6 @@ export type Wearable = (typeof WEARABLES)[number]
 export const RACES = [Race.HUMAN, Race.GNOME, Race.DWARF] as const
 export const PROFESSIONS = [Profession.WARRIOR, Profession.SAMURAI, Profession.MAGE] as const
 
-export const ROUTE = {
-  HOME: '/',
-  INVENTORY: '/inventory',
-  QUEST: '/quest',
-} as const
-
 export enum ERROR_CAUSE {
   ENTITY_NOT_EXIST,
   NOT_AVAILABLE
