abf93a59078a0f3bc799641772cdaf2e44691fca
lukas <machala.lukas@post.cz>
2024-12-27 01:27:41 +0100

    feat: umami removal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { cn } from '@/lib/utils'
-import Script from 'next/script'
 import { MedievalSharp } from 'next/font/google'
 
 import { ThemeProvider } from '@/context/theme-provider'
@@ -22,11 +21,6 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: Readonly<React.PropsWithChildren>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <head>
-        {process.env.NODE_ENV === 'production' && (
-          <Script defer src='https://cloud.umami.is/script.js' data-website-id='94bf505a-895c-4213-8e6a-0d2513987607' />
-        )}
-      </head>
       <body
         className={cn(
           'flex h-screen w-screen flex-col bg-background bg-cover font-sans antialiased',
