a8f2b32ac79e80df0dbbc2f205379b5c4e56a3c1
lukas <machala.lukas@post.cz>
2025-04-21 11:46:44 +0200

    feat: adds potions efficiency server translates

--- app/(game)/world/_components/Info/Place/Hospital/Potions.tsx
+++ app/(game)/world/_components/Info/Place/Hospital/Potions.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import React from 'react'
 import type { Potion } from '@prisma/client'
 import { useCommonShowQuery } from '@/hooks/api/use-common'
 import { useHospitalBuyPotionMutation, useHospitalShowQuery } from '@/hooks/api/use-hospital'
