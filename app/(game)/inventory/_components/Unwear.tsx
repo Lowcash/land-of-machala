347958621779c4345a49d4ea8eb3bfaaa77cbb9c
lukas <machala.lukas@post.cz>
2025-04-25 00:46:01 +0200

    fix: wear/unwear hasCombat bug

--- app/(game)/inventory/_components/Unwear.tsx
+++ app/(game)/inventory/_components/Unwear.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { usePlayerShowQuery } from '@/hooks/api/use-player'
+import { useGameShowInfoQuery } from '@/hooks/api/use-game'
 import { useWearableUnwearMutation } from '@/hooks/api/use-wearable'
 import type { MutationInput } from '@/lib/utils'
 
@@ -10,14 +10,14 @@ import { RxCross1 } from 'react-icons/rx'
 interface Props extends MutationInput<typeof useWearableUnwearMutation> {}
 
 export default function Unwear(p: Props) {
-  const playerShowQuery = usePlayerShowQuery()
-  
+  const gameShowInfoQuery = useGameShowInfoQuery()
+
   const wearableUnwearMutation = useWearableUnwearMutation()
 
   const handleUnwear = () => wearableUnwearMutation.mutate(p)
 
   return (
-    <Button variant='destructive' disabled={playerShowQuery.data?.hasCombat} onClick={handleUnwear}>
+    <Button variant='destructive' disabled={gameShowInfoQuery.derived.hasCombat} onClick={handleUnwear}>
       <RxCross1 />
     </Button>
   )
