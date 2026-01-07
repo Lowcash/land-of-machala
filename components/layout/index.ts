0af86b9b741b7e13b3f03e35d9b77c213b8a1069
lukas <machala.lukas@post.cz>
2026-01-07 22:57:21 +0100

    refactor(layout): update character page route and add new layout components
    - Wrap CharacterPanel with PageTemplate in character route
    - Add dynamic route export to prevent SSR issues
    - Update layout exports to include BasePanel, ContentCard, MobileOverlay, SplitView, StatDisplay
    - Fix CharacterPanel icon issue (Server Component can't pass functions)

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -2,5 +2,10 @@
  * Layout components for consistent UI structure
  */
 
+export { BasePanel } from './BasePanel'
+export { ContentCard } from './ContentCard'
 export { GameLayout } from './GameLayout'
+export { MobileOverlay } from './MobileOverlay'
 export { PageTemplate } from './PageTemplate'
+export { SplitView } from './SplitView'
+export { ProgressBar, StatDisplay } from './StatDisplay'
