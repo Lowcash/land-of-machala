dabcdbd0247ac405f8a3369d972f2fd2f4e1dc0c
lukas <machala.lukas@post.cz>
2026-02-23 07:40:42 +0100

    refactor: remove Card wrapper from List story example

--- src/components/ui/core/list.stories.tsx
+++ src/components/ui/core/list.stories.tsx
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { MutedText } from '../prefabs/typography/shared'
 import { Badge } from './badge'
-import { Card } from './card'
 import { List, ListItem } from './list'
 import { Text } from './typography'
 
@@ -16,24 +15,16 @@ export default meta
 
 export const Default: StoryObj<typeof List> = {
   render: () => (
-    <Card gap="md" padding="lg">
-      <Card.Header>
-        <Card.Title>Status Updates</Card.Title>
-      </Card.Header>
-
-      <Card.Content>
-        <List>
-          <ListItem icon={<Badge>New</Badge>}>
-            <Text>System optimization complete.</Text>
-          </ListItem>
-          <ListItem icon={<Badge>Update</Badge>}>
-            <MutedText>Magic scaling adjusted.</MutedText>
-          </ListItem>
-          <ListItem icon={<Badge>Fix</Badge>}>
-            <Text>Bug #123 resolved.</Text>
-          </ListItem>
-        </List>
-      </Card.Content>
-    </Card>
+    <List>
+      <ListItem icon={<Badge>New</Badge>}>
+        <Text>System optimization complete.</Text>
+      </ListItem>
+      <ListItem icon={<Badge>Update</Badge>}>
+        <MutedText>Magic scaling adjusted.</MutedText>
+      </ListItem>
+      <ListItem icon={<Badge>Fix</Badge>}>
+        <Text>Bug #123 resolved.</Text>
+      </ListItem>
+    </List>
   ),
 }
